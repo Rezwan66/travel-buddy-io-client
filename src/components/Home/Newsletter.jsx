e27e8c989e06064fd1487af60e8f187adee70e3a
Rezwan66 <shaikhrezwan66@gmail.com>
2024-01-06 07:46:34 +0600

    resolved map width issue on small devices

--- src/components/Home/Newsletter.jsx
+++ src/components/Home/Newsletter.jsx
@@ -1,11 +1,11 @@
 const Newsletter = () => {
   return (
-    <div className=" mt-20">
+    <div className="mt-20">
       <div className="py-14 bg-blue-50 dark:bg-slate-800">
         <div className="container mx-auto max-w-7xl px-6 lg:px-0 space-y-8 text-gray-500 md:px-12">
           <div className="justify-center text-center gap-6 md:text-left md:flex lg:items-center lg:gap-16">
             <div className="order-last mb-6 space-y-6 md:mb-0 md:w-6/12 lg:w-6/12">
-              <h1 className="text-4xl text-gray-400 font-bold md:text-5xl">
+              <h1 className="text-3xl text-gray-400 font-bold md:text-5xl">
                 Subscribe to our{' '}
                 <span className="text-blue-500">Newsletter</span> today
               </h1>
@@ -13,10 +13,10 @@ const Newsletter = () => {
                 Get news about our awesome services and industry specialists
                 right in your inbox.
               </p>
-              <div className="flex flex-row-reverse flex-wrap justify-center gap-4 md:gap-6 md:justify-end">
+              <div className="flex flex-row-reverse flex-wrap justify-center gap-4 md:gap-6 md:justify-end ">
                 <div className="join">
                   <input
-                    className="input input-bordered join-item"
+                    className="input input-bordered join-item w-[150px] md:w-full"
                     placeholder="Email"
                   />
                   <button className="btn btn-secondary join-item rounded-r-full">
